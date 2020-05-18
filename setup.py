18eb3d22b23c9206be50c1847b0a04172217f57f
satoshi kishimoto <kishimoto@forces11.co.jp>
2020-05-19 01:18:45 +0900

    pipでインストールできる構成に変更

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='cud_colors',
+    version='0.1.0',
+    author='kissiy179@gmail.com',
+    url='https://github.com/kissiy179/cud_colors',
+    py_modules=['cud_colors'],
+    include_package_data=True,
+    python_requires=">=2.7",
+    install_requires=[
+    ]
+)
