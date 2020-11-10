f277ee3518b0a4bb97b210378bce7af638696465
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 19:32:58 +0900

    パッケージ間で構成を統一するためにpyファイルをpythonフォルダに移動

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ package_name = os.path.basename(os.path.dirname(__file__))
 setup(
     name=package_name,
     version='0.1.0',
-    py_modules=[splitext(basename(path))[0] for path in glob('*.py')],
+    py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
     include_package_data=True,
     python_requires=">=2.7",
     install_requires=[
