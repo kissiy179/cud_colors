171544bb38a0a48c8c611b94b2e8072096b4a3df
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 19:11:07 +0900

    setup.py最小構成模索のためauthorを削除してみる

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup
 setup(
     name='cud_colors',
     version='0.1.0',
-    author='kissiy179@gmail.com',
     url='https://github.com/kissiy179/cud_colors',
     py_modules=['cud_colors'],
     include_package_data=True,
