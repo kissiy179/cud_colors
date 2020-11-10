e1f75717ed33861b46328ce63896e0c39cf0e7e1
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 19:23:19 +0900

    setup.pyのpy_modulesに全pyファイルを指定するように変更してみる

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 import os
 from setuptools import setup
+from glob import glob
 
 package_name = os.path.basename(os.path.dirname(__file__))
 
 setup(
     name=package_name,
     version='0.1.0',
-    url='https://github.com/kissiy179/cud_colors',
-    py_modules=['cud_colors'],
+    py_modules=[splitext(basename(path))[0] for path in glob('*.py')],
     include_package_data=True,
     python_requires=">=2.7",
     install_requires=[
