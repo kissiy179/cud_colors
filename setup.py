a3047ac9feb8f5da983cc7c88e02bfb147105be6
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 19:39:20 +0900

    setup.pyのがpyファイルをうまく拾えていなかったのを修正

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ import os
 from setuptools import setup
 from glob import glob
 
-package_name = os.path.basename(os.path.dirname(__file__))
+dir_path = os.path.dirname(__file__)
+package_name = os.path.basename(dir_path)
+module_names = [splitext(basename(path))[0] for path in glob('{}/python/*.py'.format(dir_path))]
 
 setup(
     name=package_name,
     version='0.1.0',
-    py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
+    py_modules=module_names,
     include_package_data=True,
     python_requires=">=2.7",
     install_requires=[
