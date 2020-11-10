ac3a9e3aa0f35945f062a41f3dae44f7f02e844b
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 20:34:57 +0900

    setup.pyの不要になった部分を削除

--- setup.py
+++ setup.py
@@ -11,15 +11,6 @@ def _requires_from_file(filename):
     
     return open(filename).read().splitlines()
 
-# dir_path = dirname(__file__)
-# package_name = basename(dir_path)
-# module_names = [splitext(basename(path))[0] for path in glob(r'{}\python\*.py'.format(dir_path))]
-# requires_packages = _requires_from_file(r'{}\requirements.txt'.format(dir_path))
-
-# print(package_name)
-# print(module_names)
-# print(requires_packages)
-
 setup(
     name='cud_colors',
     version='0.1.0',
