3c075dd884bbbaf525dc4272e57c834de50674eb
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 20:31:37 +0900

    setup.pyのpy_modulesとinstall_requiresを動作していた他のパッケージのものからペーストしてみる

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-from os.path import dirname, basename, splitext, exists
 from glob import glob
+from os.path import basename
+from os.path import splitext
+from os.path import exists
 from setuptools import setup
+from setuptools import find_packages
 
 def _requires_from_file(filename):
     if not exists(filename):
@@ -24,10 +24,9 @@ setup(
     name='cud_colors',
     version='0.1.0',
     package_dir={"": "python"},
-    py_modules=['cud_colors'],
+    py_modules=[splitext(basename(path))[0] for path in glob('python/*.py')],
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7",
-    install_requires=[
-    ]
+    install_requires=_requires_from_file('requirements.txt'),
 )
