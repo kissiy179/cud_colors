b21bc40b3cc4854113ba122e3700337627451fdb
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 20:22:20 +0900

    setup.pyのname, py_modulesを手書きに戻してみる

--- setup.py
+++ setup.py
@@ -16,15 +16,15 @@ package_name = basename(dir_path)
 module_names = [splitext(basename(path))[0] for path in glob(r'{}\python\*.py'.format(dir_path))]
 requires_packages = _requires_from_file(r'{}\requirements.txt'.format(dir_path))
 
-# print(package_name)
-# print(module_names)
-# print(requires_packages)
+print(package_name)
+print(module_names)
+print(requires_packages)
 
 setup(
-    name=package_name,
+    name='cud_colors',
     version='0.1.0',
     package_dir={"": "python"},
-    py_modules=module_names,
+    py_modules=['cud_colors'],
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7",
