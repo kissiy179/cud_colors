e6d2ce675f7c5d80cc5d461811b4d08864850cb8
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 19:42:31 +0900

    setup.pyのがpyファイルをうまく拾えていなかったのを修正

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-from setuptools import setup
+from os.path import dirname, basename, splitext
 from glob import glob
+from setuptools import setup
 
-dir_path = os.path.dirname(__file__)
-package_name = os.path.basename(dir_path)
-module_names = [splitext(basename(path))[0] for path in glob('{}/python/*.py'.format(dir_path))]
+dir_path = dirname(__file__)
+package_name = basename(dir_path)
+module_names = [splitext(basename(path))[0] for path in glob('{}\python\*.py'.format(dir_path))]
 
 setup(
     name=package_name,
