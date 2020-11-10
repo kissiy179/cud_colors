57b5b693e67af036afd93cc24b691ac845ddf483
satoshi_kishimoto <kishimoto@forces11.co.jp>
2020-11-10 20:27:00 +0900

    setup.pyのpy_modulesを手書きに戻してみる

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='cud_colors',
     version='0.1.0',
     package_dir={"": "python"},
-    py_modules=requires_packages,
+    py_modules=['cud_colors'],
     include_package_data=True,
     zip_safe=False,
     python_requires=">=2.7",
